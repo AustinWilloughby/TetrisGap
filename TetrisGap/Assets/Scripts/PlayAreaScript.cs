efb1618cb63812d3a3a60cf130a932717b8afdd4
Austin Willoughby <arwigm@rit.edu>
2019-05-12 13:04:45 -0400

    Basic Playfield Working
    Created a programatically generated playfield grid that allows users to toggle logical elements of a grid. Next step is to create a "goal" that the user needs to match in order to succeed.

---
+++ TetrisGap/Assets/Scripts/PlayAreaScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAreaScript : MonoBehaviour
+{
+    [SerializeField]
+    private byte xSize = 6;
+
+    [SerializeField]
+    private byte ySize = 9;
+
+    [SerializeField]
+    private GameObject toggleableObjectPrefab;
+    
+
+    private GameObject anchorObject;
+    private GameObject[,] toggleableObjects;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anchorObject = gameObject.transform.parent.gameObject;
+        toggleableObjects = new GameObject[xSize, ySize];
+        transform.localScale = new Vector3(xSize * 100, ySize * 100, 1);
+
+        //Calculate the offset of the bottom left corner from the center of the playfield
+        //First calculate the halfwidth (and make it negative to go down and left)
+        //Then use mod to determine if the number is odd or even.
+        //If it is even, we need to add 0.5.
+        float xOffset = -xSize / 2 + (((xSize + 1)) % 2 / 2.0f);
+        float yOffset = -ySize / 2 + (((ySize + 1)) % 2 / 2.0f);
+
+        for (byte x = 0; x < xSize; x++)
+        {
+            for (byte y = 0; y < ySize; y++)
+            {
+                toggleableObjects[x, y] = Instantiate(toggleableObjectPrefab);
+                toggleableObjects[x, y].transform.parent = anchorObject.transform;
+                toggleableObjects[x, y].transform.position = new Vector3(xOffset + x, yOffset + y);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TetrisGap/Assets/Scripts/ToggleableScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleableScript : MonoBehaviour
+{
+    [SerializeField]
+    private Color defaultColor;
+
+    [SerializeField]
+    private Color mouseOverColor;
+
+    [SerializeField]
+    private Color selectedColor;
+
+    private SpriteRenderer sRenderer;
+
+    private bool currentlySelected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+        sRenderer.color = defaultColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        sRenderer.color = mouseOverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        SetColorFromState();
+    }
+
+    private void OnMouseDown()
+    {
+        SetColorFromState();
+        currentlySelected = !currentlySelected;
+    }
+
+    void SetColorFromState()
+    {
+        if (currentlySelected)
+        {
+            sRenderer.color = selectedColor;
+        }
+        else
+        {
+            sRenderer.color = defaultColor;
+        }
+    }
+
+}
